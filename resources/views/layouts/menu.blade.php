d49c5d081772f00d0fe175cbe6a5c7377ab2a914
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-10-26 00:04:46 +0700

    deleted user2s

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -10,11 +10,4 @@
 <li class="{{ Request::is('users*') ? 'active' : '' }}">
     <a href="{!! route('users.index') !!}"><i class="fa fa-user"></i><span>Users</span></a>
 </li>
-<li class="nav-item">
-    <a href="{{ route('user2s.index') }}"
-       class="nav-link {{ Request::is('user2s*') ? 'active' : '' }}">
-        <p>User2S</p>
-    </a>
-</li>
-
 
